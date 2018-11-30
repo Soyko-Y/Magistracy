f313b2f1dd58f4218ad48498766a9a9e749fcdc0
Yury Soika <ysoi@epolsoft.com>
2018-11-30 17:46:31 +0300

    Added news content

--- frontend/src/app/news/news.module.ts
+++ frontend/src/app/news/news.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OverviewComponent } from './overview/overview.component';
-
-
 import { RouterModule } from '@angular/router';
+
+import { OverviewComponent } from './overview/overview.component';
 import { NewsService } from './news.service';
 import { MaterialModule } from '../material.module';
+
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule,
-        MaterialModule
-    ],
-    providers: [],
-    declarations: [OverviewComponent]
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule
+  ],
+  providers: [],
+  declarations: [OverviewComponent]
 })
 export class NewsModule {
 }
--- frontend/src/app/news/news.service.ts
+++ frontend/src/app/news/news.service.ts
@@ -1,45 +1,60 @@
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 // import {News} from './classes/news.class';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Injectable()
 export class NewsService {
 
+  api_key = 'c496d73da3574f508c2523648249849c';
   /**
    * Constructor
    */
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) { }
 
   /**
-   * Retrieves data for selected report
+   * Retrieves news from API
    */
-//   getReport(type: string, from: Date, to: Date): Observable<Report> {
-//     return this.http.post(Constants.BASE_URL + '/report', {from: from.getTime(), to: to.getTime(), type: type})
-//       .map(res => res['payload']);
-//   }
+  initSources() {
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+  }
 
-//   /**
-//    * Retrieves all reports as array
-//    */
-//   getReportsList() {
-//     return this.http.get(Constants.BASE_URL + '/report/list')
-//       .map((res: any) => {
-//         return res['payload'];
-//       });
-//   }
+  initArticles() {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+  }
 
-//   /**
-//    * Retrieves data for chart as array
-//    */
-//   getChart(from: Date, to: Date, columnsList: Array<String>, type: string): Observable<any> {
-//     return this.http.post(Constants.BASE_URL + '/report/overview', {
-//       from: from.getTime(),
-//       to: to.getTime(),
-//       columnsList: columnsList,
-//       type: type
-//     })
-//       .map(res => res['payload']);
-//   }
+  getArticlesByID(source: String) {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
+  //   getReport(type: string, from: Date, to: Date): Observable<Report> {
+  //     return this.http.post(Constants.BASE_URL + '/report', {from: from.getTime(), to: to.getTime(), type: type})
+  //       .map(res => res['payload']);
+  //   }
+
+  //   /**
+  //    * Retrieves all reports as array
+  //    */
+  //   getReportsList() {
+  //     return this.http.get(Constants.BASE_URL + '/report/list')
+  //       .map((res: any) => {
+  //         return res['payload'];
+  //       });
+  //   }
+
+  //   /**
+  //    * Retrieves data for chart as array
+  //    */
+  //   getChart(from: Date, to: Date, columnsList: Array<String>, type: string): Observable<any> {
+  //     return this.http.post(Constants.BASE_URL + '/report/overview', {
+  //       from: from.getTime(),
+  //       to: to.getTime(),
+  //       columnsList: columnsList,
+  //       type: type
+  //     })
+  //       .map(res => res['payload']);
+  //   }
 }
